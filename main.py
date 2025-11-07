ca4abac4cb275cb492c7c7b3f2d893da77399ab0
1tccsinan <csipes.nandi77@gmail.com>
2025-11-07 10:13:32 +0100

    file beolvasasa

---
+++ main.py
@@ -0,0 +1,15 @@
+#1. Adatok beolvasása listába
+adatok = []
+with open("data.txt", "r",encoding="utf-" ) as fin:
+    for sor in fin:
+        adatok.append(int(sor))
+
+print(adatok)
+
+#2. Adatok átlaga
+#3. Döntsük el, hogy volt-e 4es
+#4. Keressük meg, volt e 5ös
+#5. Hány darab kilences volt?
+#6. Mennyi a legnagyonn beírt szám
+#7. Hanyadik indexen van a legkisebb elem?
+#8. Páros számok kiírása paros.txt-be
